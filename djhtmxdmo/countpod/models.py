2ed52b7cfb95a5d604419a3c4e15067acc3fce3b
Richard Shea <rshea@thecubagroup.com>
2024-11-19 19:52:05 +1300

    Countpod migrations

--- djhtmxdmo/countpod/models.py
+++ djhtmxdmo/countpod/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class CountLocation(models.Model):
+    '''
+    Represents an enclosed space access to which is limited by
+    a number of, for instance, doors. 
+    '''
+    description_of_count_location = models.CharField(max_length=200)
+
+
+class CountPoint(models.Model):
+    '''
+    Represents a point of access to a CountLocation, typically 
+    a door.
+    '''
+    count_location = models.ForeignKey(CountLocation, on_delete=models.CASCADE)
+    description_of_count_point = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
