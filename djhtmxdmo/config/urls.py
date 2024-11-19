bf2a139c61e132b1c47f9f80347eccbe057b1f0d
Richard Shea <rshea@thecubagroup.com>
2024-11-19 19:36:38 +1300

    Outline of the countpod ('count with plain old django') app

--- djhtmxdmo/config/urls.py
+++ djhtmxdmo/config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todospod/', include('todospod.urls')),
+    path('countpod/', include('countpod.urls')),
 ]
--- djhtmxdmo/countpod/views.py
+++ djhtmxdmo/countpod/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, f"todospod/index.html", {})
+    return render(request, f"countpod/index.html", {})
 
